d32db0a0b32e4c840771951643a3c69894b0730c
zihad868 <zihadh868@gmail.com>
2024-02-08 16:52:25 +0600

    Update Code

--- third.js
+++ third.js
@@ -1,16 +1,21 @@
 
 function deleteInvalids(array){
-    freshArray = []
+
+    if(Array.isArray(array)){
+        freshArray = []
     
-    for(let arrayEle of array){
-        if(typeof arrayEle === 'number' && !isNaN(arrayEle)){
-            freshArray.push(arrayEle);
+        for(let arrayEle of array){
+            if(typeof arrayEle === 'number' && !isNaN(arrayEle)){
+                freshArray.push(arrayEle);
+            }
         }
+    
+        return freshArray;
     }
 
-    return freshArray;
+    return "Invalid"
 }
 
-let freshArr = deleteInvalids(['1' , {num:2} , NaN, -8])
+let freshArr = deleteInvalids([ 1 , 2 , -3 ])
 
 console.log(freshArr)
